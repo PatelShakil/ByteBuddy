b97bb65971db3ac54aa39c58e451b690cc7de7d8
PatelShakil <patelsakib95@gmail.com>
2024-09-25 23:23:21 +0530

    Play Store Production v9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.shakilpatel.notesapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
